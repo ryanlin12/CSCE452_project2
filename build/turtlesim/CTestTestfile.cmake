c485c959883797615457238ad5aa914a95fc54aa
ryanlin12 <rl386170@gmail.com>
2023-02-24 18:37:49 -0600

    initial commit

---
+++ build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ryanlin12/CSCE452_project2/src/ros_tutorials/turtlesim
+# Build directory: /home/ryanlin12/CSCE452_project2/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtlesim__py")
---
+++ build/turtlesim/turtlesim__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ryanlin12/CSCE452_project2/build/turtlesim/turtlesim__py
+# Build directory: /home/ryanlin12/CSCE452_project2/build/turtlesim/turtlesim__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
